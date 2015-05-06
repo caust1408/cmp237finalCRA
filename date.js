72b70f9c715896853a7f3870a79d710c67faa999
Craig <cav9871@saintjoe.edu>
2015-05-06 13:26:40 -0400

    Added my language from data.js

--- date.js
+++ date.js
@@ -2,5 +2,8 @@
 
 var x = new Date(2015,4,6).getTime();
 print(x);
+var temp = 86400000 * 30;
+var real = new Date(temp + x);
 var y = new Date(1430884800000);
-print(y);
+print('This is the data before' + y);
+print('This is the date after 30 days' + real);
